e53bcde4a9ab07157cb29ff7f6a16cea678c5754
Racso <oscarhs0576@gmail.com>
2026-01-01 20:27:01 -0400

    updated

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
-
-const colors = document.getElementById('container-color')
 const magenta = document.getElementById('magenta')
 const black = document.getElementById('black')
 const yellow = document.getElementById('yellow')
 const green = document.getElementById('green')
 const purple = document.getElementById('purple')
 const inputUpdated = document.getElementById('input-color')
-   
+const selectItem =document.getElementById('nameList')
+const btn = document.getElementById('btn')  
+let postArray =[]
+let html=''
+let info={}
+let pushUpdated={}
+function renderPost(){
+fetch("https://www.thecolorapi.com/scheme?hex=24B1E0&mode=triad&count=6")
+.then(resp=>resp.json())
+.then(data=> {  
+postArray.push(data)  
+html +=`
+<div>${postArray}</div>
+`   
+document.getElementById('post').innerHTML =`<img src="${html.image}">` 
+ })
+
+info={
+ colors:inputUpdated.value, 
+ mode:selectItem.value,  
+ count:5
+ }
+
+}
+btn.addEventListener('click', function(){
+fetch("https://www.thecolorapi.com/scheme?hex=24B1E0&mode=triad&count=6", info)
+.then(resp=>resp.json())
+.then(post=> {
+pushUpdated={
+    method: 'PUT', 
+    headers:{"Content-type":"application/json"},
+    body:JSON.stringify(post)
+}
+  
+  postArray.unshift(pushUpdated)
+  
+inputUpdated.value=''
+selectItem.value=''
+console.log(postArray)
+ renderPost()
+ })  
+renderPost()
+})
+
 green.addEventListener("click", function(e){
   const input = document.querySelector('input')
     if(input){
     input.style.color = e.target.value
     inputUpdated.value ="#AAD1B6"
+    
+    
     }  
 })    
 magenta.addEventListener("click",  function(e){
@@ -19,44 +62,30 @@ magenta.addEventListener("click",  function(e){
     if(input){
     input.style.color = e.target.value
     inputUpdated.value ="#F55A5A"
+   
     }  
 })    
 black.addEventListener("click",   function(e){
   const input = document.querySelector('input')
     if(input){
     input.style.color = e.target.value
-    inputUpdated.value ="#2B283A"
+    inputUpdated.value   ="#2B283A"
+      
     }  
 })    
 yellow.addEventListener("click",   function(e){
   const input = document.querySelector('input')
     if(input){
     input.style.color = e.target.value
-    inputUpdated.value ="#FBF3AB"
+    inputUpdated.value  ="#FBF3AB"
+    
     }  
 })    
 purple.addEventListener("click",   function(e){
   const input = document.querySelector('input')
     if(input){
     input.style.color = e.target.value
-    inputUpdated.value ="#A626D3"
+    inputUpdated.value   ="#A626D3"
+   
     }  
 })  
-
-const postArray = []
-let post = ''
-fetch("https://www.thecolorapi.com/id?hex=C7A490")
-.then(resp=>resp.json())
-.then(data=>{
-    
-    const opt = data.image;
-    const named = opt.named;
-    const mode = data.mode;
-   document.getElementById('monocromatico').innerHTML=`<img src="${named}"/> ` 
-    
-    
-       
-        
-     
-   
-})
