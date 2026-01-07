28787a2250fd55f475db0b720a51a397e0eec8a9
Racso <oscarhs0576@gmail.com>
2026-01-07 13:29:41 -0400

    Fix container element and update color text display

--- index.js
+++ index.js
@@ -7,17 +7,18 @@ const selectItem =document.getElementById('nameList').value.toLowerCase()
 fetch(`https://www.thecolorapi.com/scheme?hex=${inputUpdated}&mode=${selectItem}&count=5`)
   .then(response=>response.json())
   .then(data=>{
-    const container =  document.getElementById('color-id')
+    const container =  document.getElementById('color-btn')
     const items = container.querySelectorAll('.color-bg')
-    const textItems = container.querySelectorAll('.text-color')
     for(let i =0;i<items.length; i++){
       const item = items[i]
       const textItem = items[i]
       item.style.background = data.colors[i].hex.value
-       textItem.textContent = data.colors[i].hex.value
-    }   
+      textItem.textContent = data.colors[i].hex.value
+      container.classList.add('text-colors')
+    } 
  })
  
  }
+
 
 
